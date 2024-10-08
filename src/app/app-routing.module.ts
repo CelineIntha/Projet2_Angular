7e9dbad1517e9bbf993dedb18f29765d5235d509
Céline <celine.inthaaphay@stagiairesmns.fr>
2024-10-08 19:11:57 +0200

    ✨ Création du component country-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: 'Accueil'
+    title: 'Home'
+  },
+  {
+    path: 'country',
+    component: CountryDetailComponent,
+    title: 'Country'
+  },
+  {
+    path: 'country/:countryName',
+    component: CountryDetailComponent,
+    title: 'Country Détail'
   },
   {
     path: '**', // wildcard
@@ -20,4 +31,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
