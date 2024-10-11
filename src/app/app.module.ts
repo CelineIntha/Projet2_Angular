e874244250c53b960af1255a864663e84f3518b0
Céline <celine.inthaaphay@stagiairesmns.fr>
2024-10-11 12:14:21 +0200

    🚀 Ajout de la line chart, ajout d'une méthode dans le service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, CountryDetailComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-country-detail',
@@ -8,12 +10,78 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CountryDetailComponent implements OnInit {
   countryName: string = '';
+  totalMedals: number = 0;
+  numberOfEntries: number = 0;
+  totalAthletes: number = 0;
 
-  constructor(private route: ActivatedRoute) {}
+
+  // Configuration des options pour le graphique
+  lineChartData: any[] = [];
+  view: [number, number] = [700, 400];
+
+  colorScheme: Color = {
+    name: 'MedalsColorScheme',
+    selectable: true, 
+    group: ScaleType.Ordinal,  
+    domain: ['#02838F', '#FF5733', '#33FF57'] 
+  };
+  
+  // Options pour le graphique
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Dates';
+  yAxisLabel: string = 'Number of Medals';
+  timeline: boolean = true;
+
+  constructor(private route: ActivatedRoute, private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.countryName = params['countryName'];
+    // S'abonner aux données des JO
+    this.olympicService.getOlympics().subscribe(() => {
+      // Ensuite, je recupère le nom du pays à partir des paramètres de la route
+      this.route.params.subscribe(params => {
+        this.countryName = params['countryName'];
+        this.loadCountryData(this.countryName);
+      });
     });
   }
+
+  loadCountryData(countryName: string) {
+    const { country, totalMedals } = this.olympicService.getCountryDataWithTotalMedals(countryName);
+
+    // Je vérifie si le pays existe avant de charger les données
+    if (country) {
+      this.totalMedals = totalMedals;
+      this.numberOfEntries = country.participations.length;
+      this.totalAthletes = country.participations.reduce((total, p) => total + p.athleteCount, 0);
+      this.prepareLineChartData(country);
+    } else {
+      console.error(`Aucune donnée trouvée pour le pays: ${countryName}`);
+      //TODO
+      // Gestion erreur utilisateur
+    }
+  }
+
+
+  prepareLineChartData(countryData: any) {
+    const lineData = countryData.participations.map((participation: any) => {
+      return {
+        name: participation.year.toString(), 
+        value: participation.medalsCount   
+      };
+    });
+
+    this.lineChartData = [
+      {
+        name: this.countryName,  
+        series: lineData 
+      }
+    ];
+  }
+  //TODO : Unsubscribe
 }
