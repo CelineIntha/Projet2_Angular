cc420cfb8e399edbd5af73f1ae20a93d84c8e530
Céline Inthaaphay <celine.inthaaphay@stagiairesmns.fr>
2024-10-16 19:01:42 +0200

    ⚡️Refactorisation de olympic.service.ts pour gérer la logique dans le composant country-detail.component.ts

--- src/app/pages/country-detail/country-detail.component.ts
+++ src/app/pages/country-detail/country-detail.component.ts
@@ -1,14 +1,19 @@
-import {Component, OnInit, HostListener} from '@angular/core';
+import {Component, OnInit, OnDestroy, HostListener} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Color, ScaleType} from '@swimlane/ngx-charts';
 import {OlympicService} from 'src/app/core/services/olympic.service';
+import {Subscription} from 'rxjs';
+import {OlympicCountry} from "../../core/models/Olympic";
+import {Participation} from "../../core/models/Participation";
 
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
   styleUrls: ['./country-detail.component.scss'],
 })
-export class CountryDetailComponent implements OnInit {
+export class CountryDetailComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
+
   countryName: string = '';
   totalMedals: number = 0;
   numberOfEntries: number = 0;
@@ -25,7 +30,6 @@ export class CountryDetailComponent implements OnInit {
     domain: ['#02838F', '#FF5733', '#33FF57']
   };
 
-  // Options pour le graphique
   legend: boolean = true;
   animations: boolean = true;
   xAxis: boolean = true;
@@ -36,41 +40,54 @@ export class CountryDetailComponent implements OnInit {
   yAxisLabel: string = 'Number of Medals';
   timeline: boolean = true;
 
-  constructor(private route: ActivatedRoute, private olympicService: OlympicService) {
+  constructor(
+    private route: ActivatedRoute,
+    private olympicService: OlympicService
+  ) {
   }
 
   ngOnInit(): void {
-    // Permet de gérer automatiquement la taille de la line chart en fonction de la taille de l'écran
     this.view = this.getViewSize(window.innerWidth);
 
-    // S'abonner aux données des JO
-    this.olympicService.getOlympics().subscribe(() => {
-      // Ensuite, je recupère le nom du pays à partir des paramètres de la route
-      this.route.params.subscribe(params => {
-        this.countryName = params['countryName'];
-        this.loadCountryData(this.countryName);
-      });
+    // Je m'abonne aux paramètres de la route pour récupérer le nom du pays
+    const routeSubscription = this.route.params.subscribe((params) => {
+      this.countryName = params['countryName'];
+      this.loadCountryData(this.countryName);
     });
+    this.subscription.add(routeSubscription);
   }
 
   loadCountryData(countryName: string) {
-    const {country, totalMedals} = this.olympicService.getCountryDataWithTotalMedals(countryName);
-
-    // Je vérifie si le pays existe avant de charger les données
-    if (country) {
-      this.totalMedals = totalMedals;
-      this.numberOfEntries = country.participations.length;
-      this.totalAthletes = country.participations.reduce((total, p) => total + p.athleteCount, 0);
-      this.prepareLineChartData(country);
-    } else {
-      console.error(`Aucune donnée trouvée pour le pays: ${countryName}`);
-      //TODO
-      // Gestion erreur utilisateur
-    }
+    const olympicsSubscription = this.olympicService.getOlympics().subscribe((olympicCountries: OlympicCountry[] | null) => {
+      if (olympicCountries) {
+        const selectedCountry = olympicCountries.find(country => country.country === countryName);
+        if (selectedCountry) {
+          this.totalMedals = this.calculateTotalMedals(selectedCountry);
+          this.numberOfEntries = selectedCountry.participations.length;
+          this.totalAthletes = this.calculateTotalAthletes(selectedCountry);
+          this.prepareLineChartData(selectedCountry);
+        } else {
+          console.error(`Aucune donnée trouvée pour le pays: ${countryName}`);
+          // TODO: Afficher un message d'erreur ou rediriger l'utilisateur
+        }
+      }
+    });
+    this.subscription.add(olympicsSubscription);
   }
 
-  prepareLineChartData(countryData: any) {
-    const lineData = countryData.participations.map((participation: any) => {
+  // Méthode pour calculer le total des médailles
+  calculateTotalMedals(country: OlympicCountry): number {
+    return country.participations.reduce((total, participation) => total + participation.medalsCount, 0);
+  }
+
+  // Méthode pour calculer le total des athlètes
+  calculateTotalAthletes(country: OlympicCountry): number {
+    return country.participations.reduce((total, participation) => total + participation.athleteCount, 0);
+  }
+
+  // Préparation des données pour le graphique
+  prepareLineChartData(countryData: OlympicCountry) {
+    const lineData = countryData.participations.map((participation: Participation) => {
       return {
         name: participation.year.toString(),
         value: participation.medalsCount
@@ -85,7 +102,7 @@ export class CountryDetailComponent implements OnInit {
     ];
   }
 
-  // Me permet de capturer l'écran et d'ajuster la taille de la line chart
+  // Capture l'événement de redimensionnement de la fenêtre et ajuste la taille du graphique
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     const target = event.target as Window;
@@ -93,12 +110,11 @@ export class CountryDetailComponent implements OnInit {
   }
 
   private getViewSize(width: number): [number, number] {
-    if (width <= 768) {
-      return [420, 420];
-    } else {
-      return [700, 400];
-    }
+    return width <= 768 ? [420, 420] : [700, 400];
   }
 
-  //TODO : Unsubscribe
+  ngOnDestroy(): void {
+    // Je me désabonne de tous les abonnements
+    this.subscription.unsubscribe();
+  }
 }
